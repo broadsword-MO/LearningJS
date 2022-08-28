749838dff8938d8bf509618b22f6b04ba160d763
broadsword-MO <62671314+broadsword-MO@users.noreply.github.com>
2022-08-28 14:23:50 -0500

    Moved many examples out, to more appropriate files

--- randomStuff.js
+++ randomStuff.js
@@ -1,57 +1,13 @@
-// Setup
-const recordCollection = {
-    2548: {
-        albumTitle: "Slippery When Wet",
-        artist: "Bon Jovi",
-        tracks: ["Let It Rock", "You Give Love a Bad Name"],
-    },
-    2468: {
-        albumTitle: "1999",
-        artist: "Prince",
-        tracks: ["1999", "Little Red Corvette"],
-    },
-    1245: {
-        artist: "Robert Palmer",
-        tracks: [],
-    },
-    5439: {
-        albumTitle: "ABBA Gold",
-    },
-};
-
-// Only change code below this line
-function updateRecords(records, id, prop, value) {
-    if (prop != "tracks" && value != "") {
-        records[id][prop] = value;
-    } else if (
-        prop == "tracks" &&
-        records[id].hasOwnProperty("tracks") == false
-    ) {
-        records[id][prop] = [value];
-    } else if (prop == "tracks" && value != "") {
-        records[id][prop].push(value);
-    } else if ((value = "")) {
-        delete records[id][prop];
+// Sum and return from numbers array
+const numbers = [2, 3, 5];
+function sum(numberArray) {
+    let output = 0;
+    for (const element of numberArray) {
+        output += element;
     }
-    return records;
+    return output;
 }
-
-updateRecords(recordCollection, 5439, "artist", "ABBA");
-updateRecords(recordCollection, 5439, "tracks", "This and that");
-updateRecords(recordCollection, 5439, "tracks", "The other");
-updateRecords(recordCollection, 5439, "awards", "yellow");
-updateRecords(recordCollection, 5439, "234", ["multiple"]);
-updateRecords(recordCollection, 5439, "11", ["division"]);
-updateRecords(recordCollection, 5439, "wonky", "");
-
-console.log(recordCollection);
-
-// ---------------------------
-function adding() {
-    console.log(0.1 + 0.2);
-}
-
-adding(); // 0.30000000000000004 (Ha!)
+console.log(sum(numbers)); // 10
 
 // ----------------------
 const s = [5, 7, 2];
@@ -67,24 +23,24 @@ console.log(editInPlace()); // [ 2, 5, 7 ]
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
 
-// ----------------------------
-const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+// ------------- Using spread operator ---------------
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2;
 
 arr2 = [...arr1]; // Change this line
 
-console.log(arr2);
+console.log(arr2); // [ 'JAN', 'FEB', 'MAR', 'APR', 'MAY' ]
 
 // ------------ Using 'let' -----------
-let i = "outside scope";
+let i = 'outside scope';
 console.log(`Outside scope i is: ${i}`);
 function checkScope() {
-    let i = "function scope";
+    let i = 'function scope';
     if (true) {
-        let i = "block scope";
-        console.log("Block scope i is:", i);
+        let i = 'block scope';
+        console.log('Block scope i is:', i);
     }
-    console.log("Function scope i is:", i);
+    console.log('Function scope i is:', i);
     return i; // Unnecessary?
 }
 checkScope(); // Block scope i is: block scope
@@ -100,15 +56,22 @@ console.log(a, b); // 6 8
 // -----------------------
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
-    const [a, b, ...arr] = list; 
+    const [a, b, ...arr] = list; // or
+    const [, , ...arr] = list; // this
     return arr;
 }
 const arr = removeFirstTwo(source);
 console.log(arr); // [ 3, 4, 5, 6, 7, 8, 9, 10 ]
 
+// Or using .slice()
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const remFirstTwo = (list) => list.slice(2);
+console.log(remFirstTwo(source)); // [ 3, 4, 5, 6, 7, 8, 9, 10 ]
+
 // ================== Interesting ===================
-//  From Grasshoppper app
-if (3) { // 0 here produces 4, Any other number produces 2
+//  From Grasshopper app
+if (5) {
+    // 0 here produces 4, Any other number produces 2
     if (100) {
         console.log(2);
     } else {
@@ -120,7 +83,7 @@ if (3) { // 0 here produces 4, Any other number produces 2
 
 // From fCC forum
 function thing() {
-    let arr = [1, 2, 10]
+    let arr = [1, 2, 10];
     let num = 3;
     arr.push(num);
     return arr;
@@ -128,12 +91,22 @@ function thing() {
 console.log(thing()); // [ 1, 2, 10, 3 ] (the array)
 // Whereas
 function thing() {
-    let arr = [1, 2, 10]
+    let arr = [1, 2, 10];
     let num = 3;
     return arr.push(num);
 }
 console.log(thing()); // 4 ( .push() returns the length of arr)
 
+// -------------- Oddities -------------
+function adding() {
+    console.log(0.1 + 0.2);
+}
+adding(); // 0.30000000000000004 (Ha!)
+
+// Interesting
+console.log(['1', '7', '11'].map(parseInt)); // [ 1, NaN, 3 ]
+// Should actually be written:
+console.log(['1', '7', '11'].map((num) => parseInt(num))); // [ 1, 7, 11 ]
 
 // ===================================================
 //                Debugging
@@ -168,14 +141,14 @@ console.log(matrix); // [ [ 0, 0 ], [ 0, 0 ], [ 0, 0 ] ]
 // Checks what the type of something is
 
 // JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol (new with ES6) and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
-console.log(typeof ""); // string
+console.log(typeof ''); // string
 console.log(typeof 0); // number
 console.log(typeof []); // object
 console.log(typeof {}); // object
 
 // If you think you're adding two numbers, but one is actually a string, the results can be unexpected.
 let seven = 7;
-let three = "3";
+let three = '3';
 console.log(seven + three); // 73 So this concatenates instead of adding since three is a string
 console.log(typeof seven); // number
 console.log(typeof three); // string
@@ -190,9 +163,8 @@ console.log(`Sum of array values is: ${arraySum}`); // 'Sum of array values is: 
 // ================== Falsy values ===================
 // "falsy" values: false, 0, "" (an empty string), NaN, undefined, and null.
 
-
 // ================== .map with arrow function ===================
-const materials = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
 
 console.log(materials.map((material) => material.length)); // [8, 6, 7, 9]
 
@@ -202,10 +174,10 @@ console.log(materials.map((material) => material.length)); // [8, 6, 7, 9]
 function filteredArray(arr, num) {
     let newArr = []; // Only change code below this line
     for (let i = 0; i < arr.length; i++) {
-        console.log("arr[i] = " + arr[i]);
+        console.log('arr[i] = ' + arr[i]);
         if (arr[i].indexOf(num) == -1) {
             newArr.push(arr[i]);
-            console.log("newArr = " + newArr);
+            console.log('newArr = ' + newArr);
         }
     }
     return newArr; // Only change code above this line
@@ -221,12 +193,32 @@ function filteredArray(arr, elem) {
     return newArr;
 }
 // Using ES6 syntax
-const filteredArray = (arr, elem) => arr.filter(subArr => !subArr.includes(elem));
+const filteredArray = (arr, elem) =>
+    arr.filter((subArr) => !subArr.includes(elem));
 
-console.log(filteredArray([[3, 2, 3], [1, 6, 2], [3, 13, 26], [19, 3, 9]], 3)); // [ [ 1, 6, 2 ] ]
+console.log(
+    filteredArray(
+        [
+            [3, 2, 3],
+            [1, 6, 2],
+            [3, 13, 26],
+            [19, 3, 9],
+        ],
+        3
+    )
+); // [[ 1, 6, 2 ]]
 
-
-console.log(filteredArray([[3, 2, 3], [1, 6, 2], [3, 13, 26], [19, 3, 9]], 3)); // [[ 1, 6, 2 ]]
+console.log(
+    filteredArray(
+        [
+            [3, 2, 3],
+            [1, 6, 2],
+            [3, 13, 26],
+            [19, 3, 9],
+        ],
+        3
+    )
+); // [[ 1, 6, 2 ]]
 
 // ================== .hasOwnProperty and 'in' ===================
 // users.hasOwnProperty('Alan'); // true, or
@@ -253,10 +245,10 @@ let users = {
 // Using .hasOwnProperty()
 function isEveryoneHere(userObj) {
     if (
-        userObj.hasOwnProperty("Alan") &&
-        userObj.hasOwnProperty("Jeff") &&
-        userObj.hasOwnProperty("Sarah") &&
-        userObj.hasOwnProperty("Ryan")
+        userObj.hasOwnProperty('Alan') &&
+        userObj.hasOwnProperty('Jeff') &&
+        userObj.hasOwnProperty('Sarah') &&
+        userObj.hasOwnProperty('Ryan')
     ) {
         return true;
     }
@@ -267,10 +259,10 @@ function isEveryoneHere(userObj) {
 function isEveryoneHere(userObj) {
     // Only change code below this line
     if (
-        "Alan" in userObj &&
-        "Jeff" in userObj &&
-        "Sarah" in userObj &&
-        "Ryan" in userObj
+        'Alan' in userObj &&
+        'Jeff' in userObj &&
+        'Sarah' in userObj &&
+        'Ryan' in userObj
     ) {
         return true;
     } else {
@@ -283,46 +275,19 @@ function isEveryoneHere(userObj) {
 // The every method is used to validate all of names used in conjunction with the hasOwnProperty method result in a value of true being returned during each iteration.
 // If at least one name is not found using the hasOwnProperty method, the every method returns false.
 function isEveryoneHere(userObj) {
-    return ["Alan", "Jeff", "Sarah", "Ryan"].every((name) =>
+    return ['Alan', 'Jeff', 'Sarah', 'Ryan'].every((name) =>
         userObj.hasOwnProperty(name)
     );
 }
 
 console.log(isEveryoneHere(users));
 
-// ================== Object.keys() ===================
-let users = {
-    Alan: {
-        age: 27,
-        online: false,
-    },
-    Jeff: {
-        age: 32,
-        online: true,
-    },
-    Sarah: {
-        age: 48,
-        online: false,
-    },
-    Ryan: {
-        age: 19,
-        online: true,
-    },
-};
-
-function getArrayOfUsers(obj) {
-    // Only change code below this line
-    return Object.keys(obj);
-    // Only change code above this line
-}
-
-console.log(getArrayOfUsers(users)); // [ 'Alan', 'Jeff', 'Sarah', 'Ryan' ]
 
 // ================== String reversal ===================
 // Mine
 function reverseString(str) {
     let sprStr = [...str];
-    let revStr = "";
+    let revStr = '';
     for (let i = str.length; i > 0; i--) {
         revStr += sprStr.pop();
     }
@@ -331,7 +296,7 @@ function reverseString(str) {
 
 // Or
 function reverseString(str) {
-    let reversedStr = "";
+    let reversedStr = '';
     for (let i = str.length - 1; i >= 0; i--) {
         reversedStr += str[i];
     }
@@ -340,18 +305,17 @@ function reverseString(str) {
 
 // Or 'chained'
 function reverseString(str) {
-    return str.split("").reverse().join("");
+    return str.split('').reverse().join('');
 }
 
 // Like chained, but with console.log();
 function reverseString(str) {
-    let arr = str.split("");
+    let arr = str.split('');
     console.log(arr);
     let rev = arr.reverse();
     console.log(rev);
-    str = rev.join("");
+    str = rev.join('');
     return str;
 }
 
-console.log(reverseString("Hello")); // 'olleH'
-
+console.log(reverseString('Hello')); // 'olleH'
